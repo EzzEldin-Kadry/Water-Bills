1fef4261a7f17f4d740d79f07e11c2771a5be226
EzzEldin Kadry <110013767+EzzEldin-Kadry@users.noreply.github.com>
2023-07-16 00:03:09 +0300

    Removing some comments

--- NWC_Bills/ApplicationDbContext.cs
+++ NWC_Bills/ApplicationDbContext.cs
@@ -26,9 +26,6 @@ public partial class ApplicationDbContext : DbContext
 
     public virtual DbSet<NwcSubscriptionFile> NwcSubscriptionFiles { get; set; }
 
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        //=> optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=water_consumption_bill;Trusted_Connection=True;");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<NwcDefaultSliceValue>(entity =>
